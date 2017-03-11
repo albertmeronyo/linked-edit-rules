899b08248630411be00b3e1c68d99683a99463fa
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2017-03-11 19:47:01 +0100

    Passing official LDN sender tests

--- src/common/stardogwrapper.py
+++ src/common/stardogwrapper.py
@@ -2,7 +2,7 @@
 import os
 import requests
 from subprocess import call, check_output
-from rdflib import Graph
+from rdflib import Graph, URIRef
 
 class StardogWrapper():
     def __init__(self, __config):
@@ -76,6 +76,13 @@ class StardogWrapper():
         #resp = Popen([self.HOME_STARDOG + 'stardog query ' + self.DB_NAME + ' ' + self.REPORT_QUERY], shell=True, stdout=PIPE)
         resp = check_output([self.HOME_STARDOG + 'stardog', 'query', self.DB_NAME, self.REPORT_QUERY])
 
+        with open('temp.out.nt', 'w') as temp_out:
+            temp_out.write(resp)
+        report_graph = Graph()
+        report_graph.parse('temp.out.nt', format='nt')
+        print 'Contents of loaded graph with consistency report:'
+        print report_graph.serialize(format='json-ld')
+
         # If there is an LDN inbox defined, send the report graph there as a linked data notification
         if self.INBOX_PATH:
             # Discover inbox location
@@ -84,19 +91,29 @@ class StardogWrapper():
             try:
                 inbox_url = r.links['http://www.w3.org/ns/ldp#inbox']['url']
             except:
-                r = requests.get(self.INBOX_PATH, headers={'accept': 'text/turtle; q=1.0, application/ld+json; q=1.0'})
+                r = requests.get(self.INBOX_PATH, headers={'accept': 'application/ld+json'})
                 g = Graph()
                 try:
-                    g.parse(r.text)
-                    qres = g.query("select distinct ?inbox where {?foo <http://www.w3.org/ns/ldp#inbox> ?inbox . }")
-                    for row in qres:
-                        inbox_url = row
+                    with open('temp.out.json', 'w') as temp_out:
+                        temp_out.write(r.text)
+                    g.parse('temp.out.json', format='json-ld')
+                    print "Parsed this from the receiver response body:"
+                    print g.serialize(format='turtle')
+                    for s,p,o in g.triples ( (None, URIRef("http://www.w3.org/ns/ldp#inbox"), None ) ):
+                        print "THIS IS MY INBOX"
+                        print o
+                        inbox_url = o
+                    # qres = g.query("select distinct ?inbox where {?foo <http://www.w3.org/ns/ldp#inbox> ?inbox . }")
+                    # for row in qres['results']['bindings']:
+                    #     print row
+                    #     inbox_url = row
                 except:
                     print "Could not discover LDN inbox in the specified URL"
 
             # Send the notification to the discovered inbox
             print 'Posting LER report graph notification to ' + inbox_url
-            r = requests.post(inbox_url, resp, headers={'Content-Type': 'text/turtle'})
+            r = requests.post(inbox_url, report_graph.serialize(format='json-ld'), headers={'Content-Type': 'application/ld+json'})
+            print r.headers
             print r.text
 
 
